6cc28d72f6dee73e90e345827c50a9fa4750fca6
jtdejesus <157234255+jtdejesus@users.noreply.github.com>
2024-06-02 12:45:33 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+        double inches = 15994;
+        double cm = inches * 2.54;
+    Console.WriteLine (cm + " centimeters");
+  }
+}
